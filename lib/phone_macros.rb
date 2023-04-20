c6f656b3bb228c885535ec4a4e1587fef5c65703
Kevin McPhillips <github@kevinmcphillips.ca>
2023-04-19 22:30:13 -0400

    Push the sort order into the associations in the framework

--- lib/phone_macros.rb
+++ lib/phone_macros.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 module PhoneMacros
   def last_completed_haiku(response)
-    response.phone_call.responses.in_order.prompt(:gather_inspiration).last.haiku_formatted_for_voice
+    response.phone_call.responses.prompt(:gather_inspiration).last.haiku_formatted_for_voice
   end
 
   def last_responses_all(response, prompt:, count:)
-    responses = response.phone_call.responses.in_order.last(count)
+    responses = response.phone_call.responses.last(count)
     return false if responses.count < count
     responses.all?{ |r| r.prompt_handle == prompt.to_s }
   end
